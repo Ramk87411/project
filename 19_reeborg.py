a5167401e2050ae7343c62c1cb5c3e6cffefcfb3
Ram Krishna <ramk87411@gmail.com>
2023-02-13 13:27:04 +0530

    reeborg added

--- 19_reeborg.py
+++ 19_reeborg.py
@@ -1,5 +1,5 @@
 
-#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 
 """
 def turn_right():
@@ -45,4 +45,26 @@ for step in range(6):
     goal_p()
 
 
+"""
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def goal_p():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+number_of_hurdles = 6
+while number_of_hurdles < 6:
+    goal_p()
+    print(number_of_hurdles)
 """
